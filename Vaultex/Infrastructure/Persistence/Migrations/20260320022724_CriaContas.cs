2d8ef92c3737a2d4b8d10e98bf85c7c9fd2f623c
Alisson Belo <alisson.chagas@outlook.com.br>
2026-03-19 23:33:47 -0300

    feat: adiciona migration CriaContas

---
+++ Vaultex/Infrastructure/Persistence/Migrations/20260320022724_CriaContas.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Vaultex.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class CriaContas : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "accounts",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Status = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false),
+                    InsertedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_accounts", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "accounts");
+        }
+    }
+}
