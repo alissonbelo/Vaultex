0beceb4204a388868af7d355680e0ac0daa16e2a
Alisson Belo <alisson.chagas@outlook.com.br>
2026-03-19 23:33:46 -0300

    feat: adiciona interface IAccountRepository no Domain

--- Vaultex/Domain/Interfaces/IAccountRepository.cs
+++ Vaultex/Domain/Interfaces/IAccountRepository.cs
@@ -1,6 +1,10 @@
-﻿namespace Vaultex.Domain.Interfaces;
+﻿using Vaultex.Domain.Entities.Accounts;
+
+namespace Vaultex.Domain.Interfaces;
 
 public interface IAccountRepository
 {
-    
+    Task<Account?> GetByIdAsync(Guid id, CancellationToken ct = default);
+    Task<List<Account>> ListAsync(CancellationToken ct = default);
+    Task AddAsync(Account account, CancellationToken ct = default);
 }
