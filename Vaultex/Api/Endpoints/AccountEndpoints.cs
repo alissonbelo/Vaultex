5c6b7f06c7e6d15cfae646a745dc1b92398a4a4e
Alisson Belo <alisson.chagas@outlook.com.br>
2026-03-19 23:33:47 -0300

    feat: adiciona endpoints de Account

--- Vaultex/Api/Endpoints/AccountEndpoints.cs
+++ Vaultex/Api/Endpoints/AccountEndpoints.cs
@@ -1,6 +1,36 @@
-﻿namespace Vaultex.Api.Endpoints;
+﻿using Vaultex.Application.UseCases.Accounts;
 
-public class AccountEndpoints
+namespace Vaultex.Api.Endpoints;
+
+public static class AccountEndpoints
 {
-    
+    public static void MapAccountEndpoints(this WebApplication app)
+    {
+        var group = app.MapGroup("/api/accounts").WithTags("Accounts");
+
+        group.MapGet("/", async (ListAccountsUseCase useCase, CancellationToken ct) =>
+        {
+            var accounts = await useCase.ExecuteAsync(ct);
+            return Results.Ok(new { data = accounts });
+        });
+
+        group.MapGet("/{id:guid}", async (Guid id, GetAccountUseCase useCase, CancellationToken ct) =>
+        {
+            try
+            {
+                var account = await useCase.ExecuteAsync(id, ct);
+                return Results.Ok(new { data = account });
+            }
+            catch (KeyNotFoundException ex)
+            {
+                return Results.NotFound(new { error = ex.Message });
+            }
+        });
+
+        group.MapPost("/", async (CreateAccountUseCase useCase, CancellationToken ct) =>
+        {
+            var account = await useCase.ExecuteAsync(ct);
+            return Results.Created($"/api/accounts/{account.Id}", new { data = account });
+        });
+    }
 }
